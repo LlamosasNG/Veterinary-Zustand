c9c6958566fbc92b4e887d35d6605cb54bd7b0b8
LlamosasNG <gonzalezllamosas07@gmail.com>
2024-12-29 20:39:05 -0600

    Implementación de la vista de detalles de un paciente

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,39 @@
+import { Patient } from "../types";
+import PatientDetailsItem from "./PatientDetailsItem";
+import { usePatientStore } from "../store";
+
+type PatientDetailsProps = {
+  patient: Patient;
+};
+
+export default function PatientDetails({ patient }: PatientDetailsProps) {
+  const deletePatient = usePatientStore((state) => state.deletePatient);
+  const getPatientById = usePatientStore((state) => state.getPatientById);
+  return (
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+      <PatientDetailsItem label={"ID"} data={patient.id} />
+      <PatientDetailsItem label={"Nombre"} data={patient.name} />
+      <PatientDetailsItem label={"Propietario"} data={patient.caretaker} />
+      <PatientDetailsItem label={"Email"} data={patient.email} />
+      <PatientDetailsItem label={"Fecha alta"} data={patient.date.toString()} />
+      <PatientDetailsItem label={"Síntomas"} data={patient.symptoms} />
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => getPatientById(patient.id)}
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
+        >
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,10 @@ import { DraftPatient, Patient } from "./types";
 
 type PatientStore = {
   patients: Patient[];
+  activeId: Patient["id"];
   addPatient: (data: DraftPatient) => void;
+  deletePatient: (id: Patient["id"]) => void;
+  getPatientById: (id: Patient["id"]) => void;
 };
 
 const createPatient = (patient: DraftPatient): Patient => {
@@ -13,10 +16,19 @@ const createPatient = (patient: DraftPatient): Patient => {
 
 export const usePatientStore = create<PatientStore>((set) => ({
   patients: [],
+  activeId: "",
   addPatient: (data) => {
     const newPatient = createPatient(data);
     set((state) => ({
       patients: [...state.patients, newPatient],
     }));
+  },
+  deletePatient: (id) => {
+    set((state) => ({
+      patients: state.patients.filter((patient) => patient.id !== id),
+    }));
+  },
+  getPatientById: (id) => {
+    activeId: id;
   },
 }));
